156ca7f77e9818878c30e392de93dfb6f41ca766
rafael-dias <rafael.arteg@gmail.com>
2024-06-19 22:16:39 -0300

    alterada estrutura da api

--- convert.py
+++ convert.py
@@ -14,46 +14,44 @@ def ler_conteudo_arquivo(caminho):
         return arquivo.read()
 
 # Estrutura para armazenar os dados
-estrutura_json = {}
+estrutura_json = []
 
 # Caminho para a pasta 'cartas'
 caminho_base = 'cartas/'
 
 # Percorre os diretórios e arquivos
-for diretorio, _, arquivos in os.walk(caminho_base):
+for indice, (diretorio, _, arquivos) in enumerate(os.walk(caminho_base)):
+    indice -=1
     nome_diretorio = os.path.basename(diretorio)
     if nome_diretorio not in estrutura_json:
-        
         if nome_diretorio != '':
-            estrutura_json[nome_diretorio] = []
-    
-    for arquivo in arquivos:
-        if arquivo.endswith('.md'):
-            caminho_completo = os.path.join(diretorio, arquivo)
-            id, nome = extrair_informacoes(arquivo)
-            conteudo = ler_conteudo_arquivo(caminho_completo)
-            estrutura_json[nome_diretorio].append({
-                'id': int(id),
-                'nome': nome,
-                'dados': conteudo
-            })
+            estrutura_json.append({"id":indice, "livro": nome_diretorio, "dados": []})
+   
 
-for key in estrutura_json:
-    estrutura_json[key].sort(key=lambda x: x['id'])
+    # Atualize o índice aqui (se necessário)
+    if nome_diretorio != '':
+        index = len(estrutura_json) -1
+        for arquivo in arquivos:
+            if arquivo.endswith('.md'):
+                caminho_completo = os.path.join(diretorio, arquivo)
+                id, nome = extrair_informacoes(arquivo)
+                conteudo = ler_conteudo_arquivo(caminho_completo)
+                estrutura_json[index]['dados'].append({
+                    'id': int(id),
+                    'nome': nome,
+                    'dados': conteudo
+                })
+
+for item in estrutura_json:
+    item["dados"].sort(key=lambda x: x["id"])
 
     import random
     random_number = random.randint(1000000000, 9999999999)
     versao = '{"versao":'+str(random_number)+'}'
 
-with open(os.path.join('api/versao.json'), 'w', encoding='utf-8') as file:
+with open(os.path.join('api/versao2.json'), 'w', encoding='utf-8') as file:
     file.write(versao)
 
 # Converte a estrutura para JSON e salva em um arquivo
-with open('api/tarot.json', 'w', encoding='utf-8') as saida_json:
+with open('api/tarot2.json', 'w', encoding='utf-8') as saida_json:
     json.dump(estrutura_json, saida_json, ensure_ascii=False, indent=4)
-
-print('JSON criado com sucesso!')
-
-
-# with open(os.path.join('api', 'tarot.json'), "w") as file:
-#     file.write(json_object)
